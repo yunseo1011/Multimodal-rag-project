ea7983e26010847aaedd9abd15fd9cf8699fb2f0
이윤서 <yunseo04aa@naver.com>
2025-12-28 23:11:35 +0900

    fix: 데이터셋 로더 JSON 키 수정

--- src/core/dataset.py
+++ src/core/dataset.py
@@ -2,7 +2,7 @@ import json
 import torch
 from PIL import Image
 from torch.utils.data import Dataset
-from src.utils.geometry import normalize_box  # utils.py에서 함수 가져오기
+from src.utils.geometry import normalize_bbox  # utils.py에서 함수 가져오기
 # training/train.py에 사용됨
 
 class LayoutLMDataset(Dataset):
@@ -39,7 +39,7 @@ class LayoutLMDataset(Dataset):
 
             words.append(text)
             # 즉시 변환해서 추가 (효율적)
-            boxes.append(normalize_box(bbox, width, height))
+            boxes.append(normalize_bbox(bbox, width, height))
 
 
         # 3. 방어 코드 (빈 문서 처리)
